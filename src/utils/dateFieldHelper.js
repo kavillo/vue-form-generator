6f333d4b3187474af4e24ac4f88d205ba75f938a
kavillo <camillo.vezzoli@gmail.com>
2020-04-14 23:27:43 +0200

    It was not possible to edit empty dates

--- src/utils/dateFieldHelper.js
+++ src/utils/dateFieldHelper.js
@@ -2,9 +2,14 @@ import fecha from "fecha";
 
 export default {
 	formatValueToField(value) {
-		if (value != null) {
-			let dt = this.schema.format ? fecha.parse(value, this.schema.format) : new Date(value);
-			return fecha.format(dt, this.getDateFormat());
+		if (value) {
+            try {
+                let dt = this.schema.format ? fecha.parse(value, this.schema.format) : new Date(value);
+                return fecha.format(dt, this.getDateFormat());
+            } catch (e) {
+                console.log('dateFieldHelper.js:formatValueToField ' + e);
+                return value;
+            }
 		}
 
 		return value;
@@ -12,7 +17,12 @@ export default {
 
 	formatValueToModel(value) {
 		if (value != null) {
-			let m = fecha.parse(value, this.getDateFormat());
+            let m = fecha.parse(value, this.getDateFormat());
+
+            if (!m) {
+                return null;
+            }
+
 			if (this.schema.format) {
 				value = fecha.format(m, this.schema.format);
 			} else {
